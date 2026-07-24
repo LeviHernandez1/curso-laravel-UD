fdfe33b39d77bdac2f362fdd8322c77bf0ba3b3a
Levi Marin Hernandez <levimh.makingsoftware@gmail.com>
2026-07-23 18:13:03 -0600

    Terminamos seccion 13 con Autenticacion de Usuarios, redireccionamiento y crear el muro, como ultima actividad se creo la manera segura de Rivisar si las credenciales son correctas, de no serlo se imprime mensaje de error, pero de ser correcto el sistema redirecciona al usuario al muro

--- devstagram/app/Http/Controllers/loginController.php
+++ devstagram/app/Http/Controllers/loginController.php
@@ -20,5 +20,14 @@ class LoginController extends Controller
             'email' => 'required|email',
             'password' => 'required',
         ]);
+        // Esta autenticacion nos dara un valor true o false por lo que se negara por si no esta autenticado por no cumplir dos validaciones
+        if(!auth()->attempt($request->only('email', 'password'))) {
+            // Redireccionamiento de no estar autenticado correctamente con back, con with introducimos el nombre de indentificacion del metodo y el mensaje que mostrar
+            // Esto es una manera de mostrar informacion en la vista.
+            return back()->with('mensaje', 'Credenciales Incorrectas');
+        }
+        // De ser correcto lo redigirimos a su perfil solicitado
+        return redirect()->route('posts.index');
     }
+    
 }
--- devstagram/resources/views/auth/login.blade.php
+++ devstagram/resources/views/auth/login.blade.php
@@ -14,6 +14,12 @@
             <form method="POST" action="{{ route('login') }}" novalidate>
                 @csrf
                 
+                @if (session('mensaje' ))
+                    <p class=" bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
+                        {{ session('mensaje') }}
+                    </p>
+                @endif
+                
                 <div class="md-5 p-2">
                     <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
                         Email
